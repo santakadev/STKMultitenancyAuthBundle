131f297aa88ff06b31acf317f9680926babc9ba9
Daniel Santamaria <santaka87@gmail.com>
2016-05-07 09:06:03 +0200

    extend AbstractFactory

--- DependencyInjection/Security/Factory/MultitenacyFactory.php
+++ DependencyInjection/Security/Factory/MultitenacyFactory.php
@@ -2,69 +2,49 @@
 
 namespace STK\MultitenacyBundle\DependencyInjection\Security\Factory;
 
-use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\SecurityFactoryInterface;
-use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\AbstractFactory;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Reference;
 
-class MultitenacyFactory implements SecurityFactoryInterface
+class MultitenacyFactory extends AbstractFactory
 {
 
-    /**
-     * Configures the container services required to use the authentication listener.
-     *
-     * @param ContainerBuilder $container
-     * @param string $id The unique id of the firewall
-     * @param array $config The options array for the listener
-     * @param string $userProvider The service id of the user provider
-     * @param string $defaultEntryPoint
-     *
-     * @return array containing three values:
-     *               - the provider id
-     *               - the listener id
-     *               - the entry point id
-     */
-    public function create(ContainerBuilder $container, $id, $config, $userProvider, $defaultEntryPoint)
-    {
-        $providerId = 'security.authentication.provider.multitenacy.'.$id;
-        $container
-            ->setDefinition($providerId, new DefinitionDecorator('multitenacy.security.authentication.provider'))
-            ->replaceArgument(0, new Reference($userProvider))
-            ->replaceArgument(2, $id)
-        ;
-
-        $listenerId = 'security.authentication.listener.multitenacy.'.$id;
-        $container
-            ->setDefinition($listenerId, new DefinitionDecorator('multitenacy.security.authentication.listener'))
-            ->replaceArgument(4, $id);
-
-        return array($providerId, $listenerId, $defaultEntryPoint);
-    }
-
-    /**
-     * Defines the position at which the provider is called.
-     * Possible values: pre_auth, form, http, and remember_me.
-     *
-     * @return string
-     */
     public function getPosition()
     {
-        return 'pre_auth';
+        return 'form';
     }
 
-    /**
-     * Defines the configuration key used to reference the provider
-     * in the firewall configuration.
-     *
-     * @return string
-     */
     public function getKey()
     {
         return 'multitenacy';
     }
 
-    public function addConfiguration(NodeDefinition $builder)
+
+    protected function createAuthProvider(ContainerBuilder $container, $id, $config, $userProviderId)
     {
+        $providerId = 'security.authentication.provider.multitenacy.'.$id;
+        $container
+            ->setDefinition($providerId, new DefinitionDecorator('multitenacy.security.authentication.provider'))
+            ->replaceArgument(0, new Reference($userProviderId))
+            ->replaceArgument(2, $this->getKey())
+        ;
+
+        return $providerId;
     }
+
+    protected function getListenerId()
+    {
+        return 'security.authentication.listener.multitenacy';
+    }
+
+    protected function createListener($container, $id, $config, $userProvider)
+    {
+        $listenerId = 'security.authentication.listener.multitenacy.'.$id;
+        $container
+            ->setDefinition($listenerId, new DefinitionDecorator('multitenacy.security.authentication.listener'))
+            ->replaceArgument(4, $id);
+        return $listenerId;
+    }
+    
 }
